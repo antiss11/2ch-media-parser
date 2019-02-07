6dc10314bb84f9c2dc0b5be317f46a25f2150b2e
antiss11 <recc1337@gmail.com>
2019-02-07 19:46:09 +0300

    Added status
    Fixed some bugs, code refactoring.

--- gui.py
+++ gui.py
@@ -6,7 +6,7 @@ from main import *
 import os.path
 
 
-class Parser:
+class ParserGui:
     def __init__(self, master):
         self.l_link = Label(master, width=20, text='URL')
         self.e_link = Entry(master, width=50)
@@ -17,7 +17,8 @@ class Parser:
         self.type_pics = Radiobutton(master, text="pics", variable=self.content_type, value=1, width=10)
         self.type_vids = Radiobutton(master, text="vids", variable=self.content_type, value=2, width=10)
         self.type_all = Radiobutton(master, text="all", variable=self.content_type, value=3, width=10)
-        self.status = Label(width=5)
+        self.status = Label(width=10, text="Status:")
+        self.status_message = Label(width=20)
         self.l_link.grid(row=0, columnspan=3, padx=5, pady=5)
         self.e_link.grid(row=1, columnspan=3, padx=5, pady=5)
         self.b_folder.grid(row=2, columnspan=3, padx=5, pady=5)
@@ -26,38 +27,46 @@ class Parser:
         self.type_vids.grid(row=4, column=1, sticky=W)
         self.type_all.grid(row=4, column=2, sticky=W)
         self.b_start.grid(row=5, columnspan=3)
-        self.status.grid(row=7, column=0, sticky=W)
+        self.status.grid(row=7, column=0, pady=5)
+        self.status_message.grid(row=7, column=1, columnspan=2, sticky=W)
+        self.handler = StatusHandler(self.status_message)
 
     def folder(self):
         folder = filedialog.askdirectory()
         self.e_folder.insert(0, folder)
 
     def get_url(self):
-        self.url = self.e_link.get()
+        url = self.e_link.get()
+        return url
 
     def get_path(self):
-        self.path = os.path.abspath(self.e_folder.get())
+        path = os.path.abspath(self.e_folder.get())
+        return path
 
     def get_content_type(self):
-        self.content_val = self.content_type.get()
+        content_val = self.content_type.get()
+        return content_val
 
     def start(self):
-        try:
-            self.get_content_type()
-            self.get_path()
-            self.get_url()
-            if self.content_val == 1:                           # If pics
-                parse_page(self.url, self.path, 'pics')
-            elif self.content_val == 2:                         # If videos
-                parse_page(self.url, self.path, 'videos')
-            elif self.content_val == 3:                         # If all
-                parse_page(self.url, self.path, 'all')
-                self.status['text'] = 'Ok!'
-        except Exception:
-            self.status['text'] = 'Error'
+        content_type = self.get_content_type()
+        if content_type is None:
+            self.handler.message("Please select content")
+        else:
+            path = self.get_path()
+            url = self.get_url()
+            try:
+                if content_type == 1:                                                             # If pics
+                    MainActions.parse_page_thread(url, path, "pics", self.status_message)
+                elif content_type == 2:                                                           # If videos
+                    MainActions.parse_page_thread(url, path, "videos", self.status_message)
+                elif content_type == 3:                                                           # If all
+                    MainActions.parse_page_thread(url, path, "all", self.status_message)
+
+            except Exception:
+                self.handler.message("Error")
 
 
 if __name__ == "__main__":
     root = Tk()
-    base = Parser(root)
+    base = ParserGui(root)
     root.mainloop()
--- main.py
+++ main.py
@@ -5,54 +5,92 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import os
+import threading
 
 VIDEO_PATTERN = "mp4$|webm$"            # Regular expression patterns for
 PICS_PATTERN = "png$|jpg$|gif$"         # video and image formats
 
 
-def get_page(url):
-    """ Function gets page by thread url and returns Error if status code is bad """
-    page = requests.get(url)
-    if page.status_code < 400:
-        return page
-    else:
-        pass
+class PageParser:
+
+    def __init__(self, url, folder_to_save):
+        self.url = url
+        self.folder_to_save = folder_to_save
+
+    def get_page(self):
+        """ Function gets page by thread url and returns Error if status code is bad """
+        page = requests.get(self.url)
+        if page.status_code < 400:
+            page = page.text
+            return page
+        else:
+            raise Exception("Page error!")
+
+    def get_links(self, page, content_type):
+        """ Parse page by content type"""
+        soup = BeautifulSoup(page, features="lxml")
+        if content_type == "pics":
+            image_blocks = soup.findAll("a", {"class": "post__image-link", "href": re.compile(PICS_PATTERN)})
+        elif content_type == "videos":
+            image_blocks = soup.findAll("a", {"class": "post__image-link", "href": re.compile(VIDEO_PATTERN)})
+        elif content_type == "all":
+            image_blocks = soup.findAll("a", {"class": "post__image-link"})
+        cutted_links = (i.get("href") for i in image_blocks)                                                    # get short url
+        full_links = ['https://2ch.hk' + i for i in cutted_links]                                               # restore full url
+        return full_links
+
+    def get_links_count(self, links):
+        num_images = len(links)
+        return num_images
+
+    def get_names(self, links):
+        """Function cuts off file names and returns it"""
+        names_list = (i.split("/")[-1] for i in links)
+        return names_list
 
 
-def get_links(page, content_type):
-    """ Parse page by content type"""
-    soup = BeautifulSoup(page.text, features="lxml")
-    if content_type == "pics":
-        image_blocks = soup.findAll("a", {"class": "post__image-link", "href": re.compile(PICS_PATTERN)})
-    elif content_type == "videos":
-        image_blocks = soup.findAll("a", {"class": "post__image-link", "href": re.compile(VIDEO_PATTERN)})
-    elif content_type == "all":
-        image_blocks = soup.findAll("a", {"class": "post__image-link"})
-    cutted_links = (i.get("href") for i in image_blocks)                                                    # get short url
-    full_links = ['https://2ch.hk' + i for i in cutted_links]                                               # restore full url
-    return full_links
+class MainActions:
+
+    @staticmethod
+    def parse_page(url, folder, content_type, label):
+        parser = PageParser(url, folder)
+        page = parser.get_page()
+        links = parser.get_links(page, content_type)
+        num_links = parser.get_links_count(links)
+        names = parser.get_names(links)
+        handler = StatusHandler(label)
+        links_dict = list(zip(links, names))
+        set_folder(folder)
+        images_ready = 0
+        for link, name in links_dict:
+            handler.message("{0}/{1}".format(images_ready, num_links))
+            download_image(link, name)
+            images_ready += 1
 
 
-def get_names(links):
-    """Function cuts off file names and returns it"""
-    names_list = (i.split("/")[-1] for i in links)
-    return names_list
+    @classmethod
+    def parse_page_thread(cls, url, folder, content_type, label):
+        thread = threading.Thread(target=MainActions.parse_page, args=(url, folder, content_type, label))
+        thread.start()
 
 
-def download_image(link, folder, name):
+class StatusHandler:
+    def __init__(self, label):
+        self.label = label
+
+    def message(self, message):
+        self.label['text'] = message
+
+
+def download_image(link, name):
     """Funtions downloads images in binary mode"""
-    os.chdir(folder)
     with open(name, 'wb') as handle:
         responce = requests.get(link, stream=True)
         for block in responce.iter_content():
             handle.write(block)
 
 
-def parse_page(page, folder_to_save, content_type):
-    """ Main function, requiers page url (2ch thread) and content type -
-    'pics', 'videos' and 'all' """
-    page = get_page(page)
-    links = get_links(page, content_type)
-    names = get_names(links)
-    for link in links:
-        download_image(link, folder_to_save, next(names))
+def set_folder(path):
+    os.chdir(path)
+
+
